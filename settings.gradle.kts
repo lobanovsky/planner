ccf2ede17dc53121bb66420a99f9f00e9b585d75
Evgeny Lobanovsky <e.lobanovsky@yandex.ru>
2026-03-11 21:48:03 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "planner"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,15 @@
+package ru.planner
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
